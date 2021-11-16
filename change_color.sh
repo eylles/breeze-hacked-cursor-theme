698c71b299022f4ed4d84c05d71681a54051cad2
eylles <ed.ylles1997@gmail.com>
2021-11-16 05:31:57 -0600

    use $0 arg and color templates from path

--- change_color.sh
+++ change_color.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+SH_DIR="$(dirname "$0")"
+
 for arg in "$@"; do
   case "$arg" in
     -o | --output)
@@ -13,15 +16,22 @@ for arg in "$@"; do
                 ;;
   esac
 done
+
 THEME_NAME=${THEME_NAME:=oomox-breeze-hacked}
 THEME_PATH=${THEME_PATH:="${HOME}"/.icons}
 THEME_PATH=${THEME_PATH}"/"${THEME_NAME}
+THEME_PRESET=$1
+
 mkdir /tmp/breeze-hacked-build
-cp ./build.sh /tmp/breeze-hacked-build
-cp -r ./src /tmp/breeze-hacked-build
-. ~/.cache/wal/colors.sh
-sed -i "s/#192629/${color0}/g ; s/#79f5f3/${color10}/g" /tmp/breeze-hacked-build/src/cursors.svg
+cp "${SH_DIR}"/build.sh /tmp/breeze-hacked-build
+cp -r "${SH_DIR}"/src /tmp/breeze-hacked-build
+
+. $THEME_PRESET
+ACCENT_BG=${ACCENT_BG:=${SEL_BG}}
+
+sed -i "s/#192629/#${BG}/g ; s/#79f5f3/#${ACCENT_BG}/g" /tmp/breeze-hacked-build/src/cursors.svg
 /tmp/breeze-hacked-build/build.sh
 echo "[Icon Theme]\nName=${THEME_NAME}\nComment=Breeze Cursor Theme" > /tmp/breeze-hacked-build/Breeze_Hacked/index.theme
+
 cp -vr /tmp/breeze-hacked-build/Breeze_Hacked/* "${THEME_PATH}"
 rm -rf /tmp/breeze-hacked-build
